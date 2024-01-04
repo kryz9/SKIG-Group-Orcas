e2788a8b4489645bd9388247aaadc5f5120ae6c3
Tahfiz <tahfizzudeenk6md16@gmail.com>
2024-01-05 03:52:38 +0800

    -page baru (measurement & Calculator)

--- EasyMath/EasyMath/main.py
+++ EasyMath/EasyMath/main.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', error=None)
 
+@app.route('/calculator')
+def calculator():
+    return render_template('calculator.html', error=None)
+
+@app.route('/measurement')
+def measurement():
+    return render_template('measurement.html', error=None)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
